10bd24be5e6b6e50836997ea293f79047d86e273
Motasim Rahman Kazmi <motasimkazmi333@gmail.com>
2019-08-06 19:45:14 +0530

    First Commit

---
+++ src/build/precache-manifest.2ba801e27772c0857401df1763f9b7a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a2feacc28b5e16ab29ad78d7d5a533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ba41a975d2781c056c4",
+    "url": "/static/css/main.1500ab9f.chunk.css"
+  },
+  {
+    "revision": "9f02fe57a36f83089491",
+    "url": "/static/js/2.6c37ac54.chunk.js"
+  },
+  {
+    "revision": "5ba41a975d2781c056c4",
+    "url": "/static/js/main.65014379.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
